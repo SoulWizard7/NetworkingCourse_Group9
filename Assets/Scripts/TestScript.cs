0b78cefe4f37ec436b11d311a47e05b51b5b8424
Tobias Rudin <tobias_rudin@hotmail.se>
2023-01-10 11:40:12 +0100

    projectile spawning bruh

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,11 +7,13 @@ using UnityEngine;
 public class TestScript : MonoBehaviour
 {
     private Multiplayer multiplayer;
+    [SerializeField] private GameObject projectilePrefab;
 
     void Start()
     {
         multiplayer = GameObject.Find("Multiplayer").GetComponent<Multiplayer>();
         multiplayer?.RegisterRemoteProcedure("MyReplicatedFunction", MyReplicatedFunction);
+        multiplayer?.RegisterRemoteProcedure("SpawnProjectileReplicated", SpawnProjectileReplicated);
     }
 
     void MyReplicatedFunction(ushort fromUser, ProcedureParameters parameters, uint callId, ITransportStreamReader processor)
@@ -21,12 +23,18 @@ public class TestScript : MonoBehaviour
         Debug.Log($"printing {leetValue} from {fromUser}");
     }
 
+    void SpawnProjectileReplicated(ushort fromUser, ProcedureParameters parameters, uint callId, ITransportStreamReader processor)
+    {
+        GameObject.Instantiate(projectilePrefab, transform.position, transform.rotation);
+    }
+
     // Update is called once per frame
     void Update()
     {
         ProcedureParameters parameters = new ProcedureParameters();
         parameters.Set("greetings", "greetings from noob player");
         parameters.Set("Leet value", 1337);
-        multiplayer?.InvokeRemoteProcedure("MyReplicatedFunction", UserId.All, parameters);
+        multiplayer?.InvokeRemoteProcedure("MyReplicatedFunction", UserId.AllInclusive, parameters);
+        multiplayer?.InvokeRemoteProcedure("SpawnProjectileReplicated", UserId.AllInclusive, parameters);
     }
 }
