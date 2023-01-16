51fcda9794b96fe30c7d529505a41b5d018656a2
Tobias Rudin <tobias_rudin@hotmail.se>
2023-01-17 00:04:00 +0100

    sort of a second draft on the main menu/lobby UI

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,14 +1,12 @@
 using Alteruna;
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    [HideInInspector] public string UserName;
-
     [SerializeField] private List<TMP_Text> _playerHUD;
     [SerializeField] private List<TMP_Text> _teamHUD;
     [SerializeField] private TMP_InputField _nameInput;
@@ -16,10 +14,14 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject _hud;
     [SerializeField] private GameObject _roomPanel;
     [SerializeField] private Multiplayer MultiplayerPrefab;
+    [SerializeField] private Button _roomButtonPrefab;
+    [SerializeField] private Button _startButton;
     
     private Multiplayer _multiplayer;
-
     private Canvas _selfCanvas;
+    private Timer _timer;
+
+    private List<Button> _roomButtons = new List<Button>();
 
     void Start()
     {
@@ -28,21 +30,40 @@ public class UIManager : MonoBehaviour
         _nameInput.onSubmit.AddListener(name =>
         {
             _multiplayer = Instantiate(MultiplayerPrefab, Vector3.zero, Quaternion.identity);
-            UserName = _nameInput.text;
+            _multiplayer.SetUsername(_nameInput.text);
             _nameInput.gameObject.SetActive(false);
             _roomPanel.SetActive(true);
-            _multiplayer.RoomListUpdated.AddListener(ShowAvailableRooms);
-            SetupMultiplayerListeners();
+            _multiplayer.Connected.AddListener(SetupMultiplayerListeners);
         });
     }
 
     void ShowAvailableRooms(Multiplayer multiplayer)
     {
-        
+        if(_roomButtons.Count > 0)
+        {
+            _roomButtons.ForEach(button =>
+            {
+                Destroy(button.gameObject);
+            });
+            _roomButtons.Clear();
+        }
+
+        foreach (var room in multiplayer.AvailableRooms)
+        {
+            Button btn = Instantiate(_roomButtonPrefab, _roomPanel.transform);
+            TextMeshProUGUI btnText = btn.GetComponentInChildren<TextMeshProUGUI>();
+            btnText.text = room.Name;
+            btn.transform.position += Vector3.down * 2.0f;
+            _roomButtons.Add(btn);
+        }
+
+        Debug.Log($"Rooms loaded! roomcount: {multiplayer.AvailableRooms.Count}");
     }
 
-    void SetupMultiplayerListeners()
+    void SetupMultiplayerListeners(Multiplayer multiplayer, Endpoint endpoint)
     {
+        Debug.Log("CONNECTED!!");
+
         _multiplayer.RoomJoined.AddListener((multiplayer, room, user) =>
         {
             Debug.Log($"{user.Name} has joined the room with roomname: {room.Name}!");
@@ -71,11 +92,27 @@ public class UIManager : MonoBehaviour
             Debug.Log($"{user.Name} has left the room!");
             _multiplayer.CurrentRoom.Users.ForEach(e => _playerHUD[_multiplayer.CurrentRoom.Users.Count - 1].text = user.Name);
         });
+
+        _startButton.onClick.AddListener(() =>
+        {
+            _multiplayer.JoinOnDemandRoom();
+            _roomPanel.SetActive(false);
+            _hud.SetActive(true);
+            _timer.Dispose();
+        });
+
+        _multiplayer.RoomListUpdated.AddListener(ShowAvailableRooms);
+
+        _timer = new Timer(RefreshAvailableRooms, null, 0, 2000);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RefreshAvailableRooms(object state)
     {
-        
+        _multiplayer.RefreshRoomList();
+    }
+
+    private void OnDestroy()
+    {
+        _timer.Dispose();
     }
 }
